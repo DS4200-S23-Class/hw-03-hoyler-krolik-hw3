4db92f4623c310651f5c109766595b3b2cb4a064
Jack Krolik <belljack17@gmail.com>
2023-02-06 13:49:00 -0500

    pushing for final comments

--- main.js
+++ main.js
@@ -3,11 +3,14 @@
 // function to change background of webpage on click
 function change_background() {
     let color = document.body.style.backgroundColor;
+    
+    // if the color is not lightblue make it lightblue
     if (color != "lightblue") {
-        document.body.style.backgroundColor = "lightblue"
+        document.body.style.backgroundColor = "lightblue";
 }
+    // otherwise make it white
     else {
-        document.body.style.backgroundColor = "white"
+        document.body.style.backgroundColor = "white";
     }
         
 
